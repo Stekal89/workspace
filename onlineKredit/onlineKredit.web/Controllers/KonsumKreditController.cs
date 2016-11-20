20981ee7551f84a86b201d03df518a0c2b0ff5b5
Thomas Stekovits <t.stekovits@hotmail.com>
2016-11-20 18:18:30 +0100

    Controller + Verweis Controller hinzugefügt, mit der 1. Ansicht (View) "Konsum-Kredit" Button funktioniert

---
+++ onlineKredit/onlineKredit.web/Controllers/KonsumKreditController.cs
@@ -0,0 +1,26 @@
+﻿using onlineKredit.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace onlineKredit.web.Controllers
+{
+    public class KonsumKreditController : Controller
+    {
+        [HttpGet]
+        public ActionResult KreditRahmen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KreditRahmen(KreditRahmenModel model)
+        {
+            return View();
+        }
+
+
+    }
+}
