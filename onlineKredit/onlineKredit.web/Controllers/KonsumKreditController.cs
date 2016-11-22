dbf5b554dedab27bc33e0b7104f487f9daaeaa0f
Thomas Stekovits <t.stekovits@hotmail.com>
2016-11-22 12:00:01 +0100

    Slider - Eingebaut, noch nicht formatiert

--- onlineKredit/onlineKredit.web/Controllers/KonsumKreditController.cs
+++ onlineKredit/onlineKredit.web/Controllers/KonsumKreditController.cs
@@ -1,6 +1,7 @@
 ﻿using onlineKredit.web.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,15 +13,98 @@ namespace onlineKredit.web.Controllers
         [HttpGet]
         public ActionResult KreditRahmen()
         {
+            Debug.Indent();
+            Debug.WriteLine("GET - KonsumKredit - KreditRahmen");
+            Debug.Unindent();
             return View();
         }
 
         [HttpPost]
         public ActionResult KreditRahmen(KreditRahmenModel model)
         {
+            Debug.Indent();
+            Debug.WriteLine("GET - KonsumKredit - KreditRahmen");
+
+            /// Speichere Daten in die BusinessLogic
+            
+            /// gehe zum nächsten Schritt
+            
+            
+            Debug.Unindent();
+
+            return RedirectToAction("FinanhzielleSituation");
+        }
+
+        [HttpGet]
+        public ActionResult FinanzielleSituation()
+        {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult FinanzielleSituation(FinanzielleSituationModel model)
+        {
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult PersoenlicheDaten()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersoenlicheDaten(PersoenlicheDatenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Arbeitgeber()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Arbeitgeber(ArbeitgeberModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KontaktDaten()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KontaktDaten(KontaktDatenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KontoInformation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KontoInformation(KontoInformationenModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ZusammenFassung()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ZusammenFassung(ZusammenFassungModel  model)
+        {
+            return View();
+        }
     }
 }
